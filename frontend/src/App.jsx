bee3cf5a8b78e717d7c8558ef1ecc78608c20439
erdemgvnc97-cloud <erdemgvnc97@gmail.com>
2025-12-17 22:20:45 +0300

    Otomatik gÃ¼ncelleme - 2025-12-17 22:20:45

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Guide from "./pages/Guide";
-import Neden from "./pages/Neden";
-import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Guide from "./pages/Guide.jsx";
+import Neden from "./pages/Neden.jsx";
+import Contact from "./pages/Contact.jsx";
 import "./index.css";
 
 export default function App() {
@@ -12,10 +12,10 @@ export default function App() {
       <Navbar />
       <main className="main-container">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/guide" element={<Guide />} />
-          <Route path="/neden" element={<Neden />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home.jsx />} />
+          <Route path="/guide" element={<Guide.jsx />} />
+          <Route path="/neden" element={<Neden.jsx />} />
+          <Route path="/contact" element={<Contact.jsx />} />
         </Routes>
       </main>
     </Router>
