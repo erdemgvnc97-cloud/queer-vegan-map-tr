7a2ea353bb9b3becb3f0358630f235a80bcbb17e
erdemgvnc97-cloud <erdemgvnc97@gmail.com>
2025-12-18 01:30:21 +0300

    Otomatik güncelleme - 2025-12-18 01:30:21

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,9 +7,9 @@ export default function Navbar() {
        <h1 className="logo">🌱 Queer Vegan Map Türkiye</h1>
        <div className="links">
          <Link to="/">Harita</Link>
-         <Link to="/">Contact</Link>
-         <Link to="/">Guide</Link>
-         <Link to="/">Neden</Link>
+         <Link to="/contact">Contact</Link>
+         <Link to="/guide">Guide</Link>
+         <Link to="/neden">Neden</Link>
          <Link to="/">Home</Link>
          <Link to="/guide">Vegan Rehberi</Link>
        </div>
